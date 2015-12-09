2ea3435c1153964fa083c6fab3cdce821b530b15
akiyo yokota <ayokota@ayokota.attlocal.net>
2015-12-08 22:35:44 -0800

    first commit, throwing away code

---
+++ ClassificationApp/src/Pair.java
@@ -0,0 +1,28 @@
+
+public class Pair {
+	private Integer first;
+	private Double second;
+	
+	public Pair(Integer first, Double second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	public Integer getFirst() {
+		return first;
+	}
+
+	public void setFirst(Integer first) {
+		this.first = first;
+	}
+
+	public Double getSecond() {
+		return second;
+	}
+
+	public void setSecond(Double second) {
+		this.second = second;
+	}
+	
+	
+}
